15b35b00b1222907aa46ab71131d9cf25d00527b
Chris Boertien <chris@aktionlab.com>
2012-11-08 04:29:31 -0700

    fix base role so it provides an array to run_list
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- roles/base.rb
+++ roles/base.rb
@@ -1,4 +1,3 @@
 name 'base'
 description 'A role for all servers to include base functionality'
-recipes = %w(users::sysadmins build-essential git ntp rsyslog logrotate python perl php vim)
-run_list recipes.map {|r| "recipe[#{r}]"}.join(' ')
+run_list %w(users::sysadmins build-essential git ntp rsyslog logrotate python perl php vim).map{|r| "recipe[#{r}]"}
