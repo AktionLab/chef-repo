48972afe8982a6a60e3a2a6b8704d1fcc04079b6
Chris Boertien <chris@aktionlab.com>
2012-11-08 03:51:17 -0700

    add users::sysadmins to base role
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name 'base'
 description 'A role for all servers to include base functionality'
-run_list 'recipe[build-essential]', 'recipe[git]', 'recipe[ntp]', 'recipe[rsyslog]', 'recipe[logrotate]', 'recipe[python]', 'recipe[perl]', 'recipe[php]'
+run_list 'recipe[users::sysadmins]', 'recipe[build-essential]', 'recipe[git]', 'recipe[ntp]', 'recipe[rsyslog]', 'recipe[logrotate]', 'recipe[python]', 'recipe[perl]', 'recipe[php]'
