d71bcc49c946219b8b0bedb58c8d8b2ff5140eb0
Chris Boertien <chris@aktionlab.com>
2012-11-08 04:33:23 -0700

    add ssh_known_hosts to base role
    Signed-off-by: Chris Boertien <chris@aktionlab.com>

--- roles/base.rb
+++ roles/base.rb
@@ -1,3 +1,3 @@
 name 'base'
 description 'A role for all servers to include base functionality'
-run_list %w(users::sysadmins build-essential git ntp rsyslog logrotate python perl php vim).map{|r| "recipe[#{r}]"}
+run_list %w(users::sysadmins build-essential git ntp rsyslog logrotate python perl php vim ssh_known_hosts).map{|r| "recipe[#{r}]"}
